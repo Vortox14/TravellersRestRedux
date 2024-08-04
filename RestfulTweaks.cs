18f38b30eb71c6c834ce6565bbf30ff027686f44
Doug <doug.stalker@gmail.com>
2024-08-04 14:54:21 +1000

    added more staff hiring features

--- RestfulTweaks.cs
+++ RestfulTweaks.cs
@@ -34,6 +34,10 @@ namespace RestfulTweaks
         private static ConfigEntry<bool> _CropFastGrow;
         private static ConfigEntry<bool> _CropFastRegrow;
         private static ConfigEntry<bool> _staffNoNeg;
+        private static ConfigEntry<bool> _staffRefreshOnOpen;
+        private static ConfigEntry<bool> _staffAlways3Perks;
+        private static ConfigEntry<int> _staffLevel;
+        private static ConfigEntry<bool> _dumpStaffGenData;
 
         private static List<Item> itemDB = new List<Item>();
 
@@ -49,6 +53,7 @@ namespace RestfulTweaks
             _itemStackSize = Config.Bind("Stacks", "Item Stack Size", -1, "Change the stack size of any item that normally stacks to 99; set to -1 to disable");
             _dumpItemListOnStart= Config.Bind("Database", "List Items on start", false, "set to true to print a list of all items to console on startup");
             _dumpRecipeListOnStart = Config.Bind("Database", "List Recipes on start", false, "set to true to print a list of all recipes to console on startup");
+            _dumpStaffGenData = Config.Bind("Database", "List staff generation data on start", false, "set to true to print a list of staff generation data on startup");
             _moveSpeed = Config.Bind("Movement", "Walking Speed", 2.5f, "walking speed; set to 2.5 for default speed ");
             _moveRunMult = Config.Bind("Movement", "Run Speed Multiplier", 1.6f, "run speed multiplier; set to 1.6 for default speed ");
             _soilStaysWatered = Config.Bind("Farming", "Soil Stays Wet", false, "Soil stays watered");
@@ -60,6 +65,10 @@ namespace RestfulTweaks
             _CropFastGrow = Config.Bind("Farming", "Fast Growing Crops", false, "All crops advance one growth stage per day");
             _CropFastRegrow = Config.Bind("Farming", "Fast Regrowing Crops", false, "Crops that allow multiple harvests can be harvested every day");
             _staffNoNeg = Config.Bind("Staff", "No Negative Perks", false, "New Staff will not have any negative perks");
+            _staffRefreshOnOpen = Config.Bind("Staff", "Refresh Applicants on Open", false, "Refresh the list of new staff available to hire every time the hiring interface is opened");
+            _staffAlways3Perks = Config.Bind("Staff", "Always Three Perks", false, "New hires will always have three positive perks");
+            _staffLevel = Config.Bind("Staff", "Staring Level", -1, "Starting level for new hires; set to -1 to disable, set to 31 for all three skills at level 5");
+
         }
 
         private void Awake()
@@ -142,10 +151,67 @@ namespace RestfulTweaks
             }
         }
 
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        // No Negative Perks on Staff
 
-        [HarmonyPatch(typeof(StaffManager), "CreateRandomOptionsWorkers")]
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Staff Generation Stuff
+
+        // Default values:
+        // reputation, prob1Perk, prob2Perk, lvlRangePerk1, lvlRangePerk2, lvlRangePerk3
+        // 7, 80, 20, (1, 2), (1, 2), (0, 0)
+        // 10, 60, 40, (1, 3), (2, 4), (0, 0)
+        // 13, 25, 60, (2, 5), (3, 5), (3, 5)
+        // 16, 0, 80, (0, 0), (4, 7), (5, 7)
+        // 19, 0, 70, (0, 0), (5, 8), (7, 10)
+        // 21, 0, 50, (0, 0), (6, 9), (9, 11)
+        // 24, 0, 40, (0, 0), (7, 10), (10, 15)
+
+
+        [HarmonyPatch(typeof(StaffManager), "Awake")]
+        [HarmonyPostfix]
+        private static void StaffManagerAwakePostfix()
+        {
+            DebugLog("StaffManager.Awake.Postfix");
+            if (_dumpStaffGenData.Value) Log.LogInfo("id, reputation, prob1Perk, prob2Perk, lvlRangePerk1, lvlRangePerk2, lvlRangePerk3");
+            StaffManager s = StaffManager.GetInstance();
+            StaffManager.StaffGenerationValues[] q = s.staffGenerationTable;
+            for (int i = 0; i < q.Length; i++)
+            {
+                if (_staffAlways3Perks.Value)
+                {
+                    q[i].prob1Perk = 0; q[i].prob2Perk = 0;
+                }
+                if (_staffLevel.Value > 0)
+                {
+                    q[i].lvlRangePerk1.x = _staffLevel.Value; q[i].lvlRangePerk1.y = _staffLevel.Value;
+                    q[i].lvlRangePerk2.x = _staffLevel.Value; q[i].lvlRangePerk2.y = _staffLevel.Value;
+                    q[i].lvlRangePerk3.x = _staffLevel.Value; q[i].lvlRangePerk3.y = _staffLevel.Value;
+                }
+                if (_dumpStaffGenData.Value) Log.LogInfo(string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}",i, q[i].reputation, q[i].prob1Perk, q[i].prob2Perk, q[i].lvlRangePerk1, q[i].lvlRangePerk2, q[i].lvlRangePerk3));
+            }
+
+
+
+        }
+
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        // Refresh Staff on every HireUI open
+
+        [HarmonyPatch(typeof(HireStaffUI), "OpenUI")]
+        [HarmonyPrefix]
+        private static void HireStaffUIOpenUIPrefix()
+        {
+            if (_staffRefreshOnOpen.Value)
+            {
+                StaffManager.CreateRandomOptionsWorkers();
+            }
+            
+        }
+
+            // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // No Negative Perks on Staff
+
+            [HarmonyPatch(typeof(StaffManager), "CreateRandomOptionsWorkers")]
         [HarmonyPostfix]
         private static void StaffManagerCreateRandomOptionsWorkersPostFix()
         {
@@ -154,7 +220,6 @@ namespace RestfulTweaks
             if (_staffNoNeg.Value)
             {
                 foreach (EmployeeInfo w in s.barworkerOptions) w.perksInfo.RemoveAt(w.perksInfo.Count - 1);
-
                 foreach (EmployeeInfo x in s.bouncerOptions) x.perksInfo.RemoveAt(x.perksInfo.Count - 1);
                 foreach (EmployeeInfo y in s.waiterOptions) y.perksInfo.RemoveAt(y.perksInfo.Count - 1);
                 foreach (EmployeeInfo z in s.houseKeeperOptions) z.perksInfo.RemoveAt(z.perksInfo.Count - 1);
